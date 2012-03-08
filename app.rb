ea848652be7190c582f7ee2ea4d01b56a2370910
Joe Fredette <jfredett@gmail.com>
2012-03-08 15:41:22 -0500

    implement faceted search

--- app.rb
+++ app.rb
@@ -1,11 +1,14 @@
 require 'sinatra'
 require 'tire'
 require 'pry'
+require 'sass'
 
 
-#def html(path)
-  #send_file File.join("views", "#{path}.html")
-#end
+def return_link(opts = {})
+  query = params.merge(opts)
+  request_string = query.map {|k,v| "#{k}=#{v}"}.join('&')
+  "#{url}?#{request_string}"
+end
 
 get '/' do
   #this will be replaced with a cloudfront-hosted page.
@@ -25,23 +28,29 @@ get '/search' do
   redirect to('/') unless params.has_key?("query") 
   query_string = params["query"]
   lang = (params[:lang] || :en).to_sym
-  filter_term = params[:filter]
+  filter_term = params[:filter_term]
 
   @results = Tire.search("_all") do
     query do
       string query_string
     end
 
-    facet 'subjects' do
+    facet "subjects_#{lang}" do
       terms "subjects_#{lang}"
     end
 
-    facet 'genres' do
+    facet "genres_#{lang}" do
       terms "genres_#{lang}"
     end
 
     facet 'type' do
       terms :type
+    end
+
+    if filter_term 
+      filter_items = filter_term.split(":")
+      facet_name = filter_items.shift
+      filter :terms, { facet_name => filter_items }
     end
 
     size 100
