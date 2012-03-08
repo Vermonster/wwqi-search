2e17bd12403d3ae5598615e8f29e0165b84faaef
Joe Fredette <jfredett@gmail.com>
2012-03-08 14:45:28 -0500

    implement search with facets

--- app.rb
+++ app.rb
@@ -24,17 +24,32 @@ end
 get '/search' do
   redirect to('/') unless params.has_key?("query") 
   query_string = params["query"]
-
-  @lang = (params[:lang] || :en).to_sym
+  lang = (params[:lang] || :en).to_sym
+  filter_term = params[:filter]
 
   @results = Tire.search("_all") do
     query do
       string query_string
     end
 
+    facet 'subjects' do
+      terms "subjects_#{lang}"
+    end
+
+    facet 'genres' do
+      terms "genres_#{lang}"
+    end
+
+    facet 'type' do
+      terms :type
+    end
+
     size 100
   end
 
+  #set up environment for the template to render
+  @lang = lang
+  @filter_term = filter_term
   @results = @results ? @results.results : [] # because Tire returns nil if the search has no results...
 
   erb :results
