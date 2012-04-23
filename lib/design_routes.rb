93fbc02dca8dafc7737c818155140769fa9906a3
Joe Fredette <jfredett@gmail.com>
2012-04-23 13:56:45 -0400

    clean up app.rb

---
+++ lib/design_routes.rb
@@ -0,0 +1,29 @@
+module DesignRoutes
+  get '/pendar/:lang/collections/manifest.html' do |lang|
+    @lang = lang
+    erb :collection_manifest
+  end
+
+  get '/pendar/:lang/collections/:id.html' do |lang, id|
+    @lang = lang
+    @id = id
+    erb :collection
+  end
+
+  get '/pendar/:lang/item/:id.html' do |lang, id|
+    @lang = lang
+    @id = id
+    erb :item
+  end
+
+
+  get '/pendar/:lang/home' do |lang|
+    @lang = lang
+    erb :home
+  end
+
+  get '/pendar/:lang/browse' do |lang|
+    @lang = lang
+    erb :browse
+  end
+end
---
+++ lib/ext.rb
@@ -0,0 +1,14 @@
+class Array
+  def all
+    self
+  end
+end
+
+def period(i)
+  OpenStruct.new(title_en: "Period #{i}",
+                 title_fa: "Period_farsi #{i}",
+                 end_at: "1900-01-01",
+                 start_at: "1800-01-01")
+end
+Period = [period(1), period(2), period(3), period(4)]
+
