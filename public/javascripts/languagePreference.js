61735815a6003b576b69929a4d25f6ac1563c859
Kathryn Bennett <kbennett13@college.harvard.edu>
2012-05-02 14:13:04 -0400

    Add language preference cookie setter

--- public/javascripts/languagePreference.js
+++ public/javascripts/languagePreference.js
@@ -1,12 +1,10 @@
 $(function () {
 
-  $('.lang-selector').click(function () {
+  $('a.lang-selector').click(function (event) {
     var langRegex = /\/(fa|en)\//;
     var matches = langRegex.exec($(this).attr('href'));
 
     if (matches[1]) { $.cookie("languagePref", matches[1]); }
-
-    alert("Cookie is " + $.cookie("languagePref"));
   });
 
 });
