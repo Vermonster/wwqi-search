62a0f90769636c99b4cc1e3daf8096ee623012b4
Joe Fredette <jfredett@gmail.com>
2012-05-04 12:25:11 -0400

    add SKIP_DATABASE flag
    for pendar to develop w/o needing a DB for translations

--- initializers/activerecord.rb
+++ initializers/activerecord.rb
@@ -1,13 +1,15 @@
 require 'active_record'
 require 'uri'
 
-db = URI.parse(URI.encode((ENV['DATABASE_URL'] || 'postgres://localhost/mydb').strip))
+unless ENV["SKIP_DATABASE"]
+  db = URI.parse(URI.encode((ENV['DATABASE_URL'] || 'postgres://localhost/mydb').strip))
 
-ActiveRecord::Base.establish_connection(
-  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  :host     => db.host,
-  :username => db.user,
-  :password => db.password,
-  :database => db.path[1..-1],
-  :encoding => 'utf8'
-)
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
