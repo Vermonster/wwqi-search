e3d37f72b12b0c51c5f1f0ef906222b25b905202
Brian Kaney <brian@vermonster.com>
2012-05-07 11:26:05 -0400

    make @lang to_sym for pendar's routes

--- lib/design_routes.rb
+++ lib/design_routes.rb
@@ -4,64 +4,64 @@ module DesignRoutes
   end
 
   get '/pendar/:lang/collections/manifest.html' do |lang|
-    @lang = lang
+    @lang = lang.to_sym
     erb :collection_manifest
   end
 
   get '/pendar/:lang/collections/:id.html' do |lang, id|
-    @lang = lang
+    @lang = lang.to_sym
     @id = id
     erb :collection
   end
 
   get '/pendar/:lang/item/:id.html' do |lang, id|
-    @lang = lang
+    @lang = lang.to_sym
     @id = id
     erb :item
   end
 
   get '/pendar/:lang/index.html' do |lang|
-    @lang = lang
+    @lang = lang.to_sym
     erb :home
   end
 
   get '/pendar/:lang/browse.html' do |lang|
-    @lang = lang
+    @lang = lang.to_sym
     erb :browse
   end
 
   get '/pendar/:lang/contact.html' do |lang|
-    @lang = lang
+    @lang = lang.to_sym
     erb :contact
   end
 
   get '/pendar/:lang/faq.html' do |lang|
-    @lang = lang
+    @lang = lang.to_sym
     erb :faq
   end
   
   get '/pendar/:lang/links.html' do |lang|
-    @lang = lang
+    @lang = lang.to_sym
     erb :links
   end
   
   get '/pendar/:lang/about.html' do |lang|
-    @lang = lang
+    @lang = lang.to_sym
     erb :about
   end
 
   get '/pendar/:lang/donate.html' do |lang|
-    @lang = lang
+    @lang = lang.to_sym
     erb :donate
   end
 
   get '/pendar/:lang/permissions.html' do |lang|
-    @lang = lang
+    @lang = lang.to_sym
     erb :permissions
   end
 
   get '/pendar/:lang/credits.html' do |lang|
-    @lang = lang
+    @lang = lang.to_sym
     erb :credits
   end
 end
